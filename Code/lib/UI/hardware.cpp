0239bc804fe6e70407126392c2cc4c6baa6785c4
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-24 23:29:08 +0200

    cleanup and mailbox purging

--- Code/lib/UI/hardware.cpp
+++ Code/lib/UI/hardware.cpp
@@ -8,38 +8,33 @@ version:            V1.2
 *****************************************************************************************
 */
 #include "hardware.h"
-#include "mailbox.h"
 #include "states.h"
 #include "tasks.h"
 
 
-static unsigned long last_interrupt_time_BUT = 0;
+static uint32_t last_interrupt_time_BUT = 0;
 
 
-void IRAM_ATTR BUT_ISR() 
-{
-    unsigned long interrupt_time = millis();
+void IRAM_ATTR BUT_ISR() {
+    uint32_t interrupt_time = millis();
     if (interrupt_time - last_interrupt_time_BUT > IR_DEBOUNCE_TIME)
     {
         cmd_t c = {
             .origin = ORG_HW,
             .content = STATE_BELL
         };
-        mbox.push(c, true);
-        mbox.notify(tLoop, true);
+        task_notify(tLoop, c, true);
     }
     last_interrupt_time_BUT = interrupt_time;
 }
 
 
-void init_ISRs(void)
-{
+void init_ISRs(void){
     attachInterrupt(BUTTON_IN_PIN, BUT_ISR, FALLING);
 }
 
 
-void init_gpios(void)
-{
+void init_gpios(void){
     pinMode(BUTTON_IN_PIN, INPUT);
     pinMode(LED_ARRAY, OUTPUT);
     pinMode(BUTTON_LED_PIN, OUTPUT);
@@ -47,25 +42,19 @@ void init_gpios(void)
 }
 
 
-void led_on(void)
-{
+void led_on(void){
     digitalWrite(BUTTON_LED_PIN, HIGH);
 }
 
 
-void led_off(void)
-{
+void led_off(void){
     digitalWrite(BUTTON_LED_PIN, LOW);
 }
 
 
-void led_blink(void)
-{
-    for(int k = 0; k < RING_TIME; k++)
-    {
-        for(int i = 0; i < 3; i++)
-        {
-            
+void led_blink(void){
+    for(int k = 0; k < RING_TIME; k++){
+        for(int i = 0; i < 3; i++){
             digitalWrite(BUTTON_LED_PIN, HIGH);
             vTaskDelay(100 / portTICK_PERIOD_MS);
             digitalWrite(BUTTON_LED_PIN, LOW);
