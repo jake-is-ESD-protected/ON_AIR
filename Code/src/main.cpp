cc48a284e8572a9962ddaa10cfc00345533e1300
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-24 23:27:16 +0200

    cleanup

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,17 +1,3 @@
-/*
-*****************************************************************************************
-project name:       ESP32_P2P_Receiver
-auth:               Jakob T.
-date:               16.01.22
-brief:              driver for ON AIR shield (acts as slave using ESP-Now)
-version:            V1.2
-*****************************************************************************************
-*/
-
-#define VERBOSE_DEBUG
-
-
-#include "mailbox.h"
 #include "lcd.h"
 #include "tasks.h"
 #include "states.h"
@@ -22,35 +8,34 @@ version:            V1.2
 void setup() {
 
   Serial.begin(115200);
-  Serial.printf("\r\n****************\r\nIDENTIFIER: SLAVE, ON AIR SHIELD\r\n****************\r\n");
+  Serial.printf("\r\n****************\r\n---ON AIR---\r\n****************\r\n");
   
   init_gpios();
+  init_ISRs();
   lcd.init_all();
   lcd.show_init_screen();
   vTaskDelay(3000 / portTICK_PERIOD_MS);
   lcd.clear();
 
-  cmd_t c = {
-    .origin = ORG_SW,
-    .content = STATE_IDLE
-  };
-
-  init_ISRs();
+  mywebserver_init();
 
   xTaskCreate(mainLoopTask,
               "main driver loop",
               4096,
               NULL,
               1,
-              &tLoop);          
+              &tLoop);
 
-  vTaskDelay(3000 / portTICK_PERIOD_MS);         
+  vTaskDelay(3000 / portTICK_PERIOD_MS);  
 
-  mbox.push(c, false);
-  mbox.notify(tLoop, false);
+    cmd_t c = {
+    .origin = ORG_SW,
+    .content = STATE_IDLE
+  };       
+
+  task_notify(tLoop, c, false);
 }
 
-
 void loop() {
-  // unused
+
 }
