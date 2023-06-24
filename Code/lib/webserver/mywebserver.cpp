ffba2ef772594ea86d33fc6dccd3dd029052630a
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-24 15:31:21 +0200

    cleanup

---
+++ Code/lib/webserver/mywebserver.cpp
@@ -0,0 +1,125 @@
+#include "mywebserver.h"
+#include "mailbox.h"
+#include "states.h"
+#include "tasks.h"
+#include "index.h"
+#include "core.h"
+#include "passwords.h"
+
+AsyncWebServer* server;
+
+
+void mywebserver_printIP()
+{
+    Serial.printf("[SRVR]\tIP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void mywebserver_init()
+{
+    uint32_t cur_time = millis();
+    Serial.printf("[SRVR]\tConnecting to %s ", SSID);
+    WiFi.begin(SSID, PWD);
+    while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        Serial.printf(".");
+    }
+    Serial.printf("\n[SRVR]\tWiFi connected on %d ms after boot.\n", 
+                (uint32_t)((millis() - cur_time) / portTICK_PERIOD_MS));
+    mywebserver_printIP();
+
+    static AsyncWebServer s(80);
+    server = &s;
+
+    // index
+    server->on("/", HTTP_POST, [](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_onConnect\n");
+        req->send(200, "text/html", ws_index);
+    });
+
+
+    // idle
+    server->on("/idle", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_idle\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_IDLE});
+    });
+
+    // woex
+    server->on("/woex", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_woex\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_WOEX});
+    });
+
+    // meet
+    server->on("/meet", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_meet\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_MEET});
+    });
+
+    // reco
+    server->on("/reco", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_reco\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_RECO});
+    });
+
+    // wait
+    server->on("/wait", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_wait\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_WAIT});
+    });
+
+    // welc
+    server->on("/welc", HTTP_POST, [&](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_welc\n");
+        core.handle_cmd({.origin=ORG_WS, .content=STATE_WELC});
+    });
+
+    // getBell
+    server->on("/getState", HTTP_GET, [&](AsyncWebServerRequest* req){
+
+        if(core.bell)
+        {
+            Serial.printf("[SRVR]\ttrying to send a bell!\n");
+            req->send(200, "text/html", "bell");
+        }
+        else
+        {
+            switch(core.last_cmd.content)
+            {
+                case STATE_IDLE:
+                req->send(200, "text/html", "idle");
+                break;
+
+                case STATE_WOEX:
+                req->send(200, "text/html", "woex");
+                break;
+
+                case STATE_MEET:
+                req->send(200, "text/html", "meet");
+                break;
+
+                case STATE_RECO:
+                req->send(200, "text/html", "reco");
+                break;
+
+                case STATE_WAIT:
+                req->send(200, "text/html", "wait");
+                break;
+
+                case STATE_WELC:
+                req->send(200, "text/html", "welc");
+                break;
+
+                default:
+                break;
+            }
+        }       
+    });
+
+    // 404
+    server->onNotFound([](AsyncWebServerRequest* req){
+        Serial.printf("[SRVR]\tIn handle_404 :(\n");
+    });
+
+    server->begin();
+}
---
+++ Code/lib/webserver/mywebserver.h
@@ -0,0 +1,11 @@
+#ifndef _MYWEBSERVER_H_
+#define _MYWEBSERVER_H_
+
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
+
+#define WEBSERVER_TIMEOUT   2000
+#define BUFLEN              30
+
+#endif
