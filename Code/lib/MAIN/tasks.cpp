57722557827b10eb052714dd71085606601a861b
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-24 23:30:30 +0200

    de-objectify and simplify

--- Code/lib/MAIN/tasks.cpp
+++ Code/lib/MAIN/tasks.cpp
@@ -1,29 +1,20 @@
 #include "tasks.h"
-#include "mailbox.h"
 #include "core.h"
 #include "mywebserver.h"
 #include "hardware.h"
 
 TaskHandle_t tLoop = NULL;
 
-void mainLoopTask(void* param)
-{
-  core.ws.init();
-  while(1)
-  {
-    mbox.wait();
-    while(mbox.data_avail())
-    {
-      core.handle_cmd(mbox.pop(NON_BLOCKING));
+void mainLoopTask(void* param){
+    while(1){
+        handle_cmd(task_notify_take(portMAX_DELAY));
     }
-  }
 }
 
 
-void time_led_task(void* param)
-{
-    core.tim_alive = true;
-    core.led_alive = true;
+void time_led_task(void* param){
+    tim_alive = true;
+    led_alive = true;
     TaskHandle_t tLED = NULL;
     xTaskCreate(blink_led_task,
                     "blink green LED",
@@ -35,48 +26,40 @@ void time_led_task(void* param)
     // check periodically if timer is not cancelled from outside       
     uint8_t i = 0;
     Serial.printf("[DEBUG]\tStarting ring-timeout\r\n");
-    while(core.tim_alive && (i < RING_AMOUNT))
-    {
+    while(tim_alive && (i < RING_AMOUNT)){
         vTaskDelay((RING_TIME / RING_AMOUNT) / portTICK_PERIOD_MS);
         i++;
     }
-    if(!core.tim_alive)
-    {
+    if(!tim_alive){
         // process forced to exit by outside means
-        core.led_alive = false;
-        core.bell = false;
+        led_alive = false;
+        bell = false;
         Serial.printf("\r\n[DEBUG]\ttimeout interrupted\r\n");
         vTaskDelete(NULL);
     }
-    else
-    {
+    else{
         // process tasked to die after time is up
         Serial.printf("\r\n[DEBUG]\ttimeout quitting\r\n");
-        core.led_alive = false;
-        core.bell = false;
+        led_alive = false;
+        bell = false;
         
-        cmd_t cmd = {
+        cmd_t c = {
             .origin = ORG_SW,
             .content = STATE_NO_RESPONSE
         };
-        mbox.push(cmd, false);
-        mbox.notify(tLoop, false);
+        task_notify(tLoop, c, false);
         vTaskDelay(DOUBLE_MSG_DELAY * 2 / portTICK_PERIOD_MS);
-        cmd = core.last_cmd;
-        mbox.push(cmd, false);
-        mbox.notify(tLoop, false);
-        core.tim_alive = false;
+        c = last_cmd;
+        task_notify(tLoop, c, false);
+        tim_alive = false;
         vTaskDelete(NULL);
     }
 }
 
 
-void blink_led_task(void* param)
-{
-    while(core.led_alive)
-    {
-        for(int i = 0; i < 3; i++)
-        {
+void blink_led_task(void* param){
+    while(led_alive){
+        for(int i = 0; i < 3; i++){
             led_on();
             vTaskDelay(100 / portTICK_PERIOD_MS);
             led_off();
@@ -88,24 +71,38 @@ void blink_led_task(void* param)
 }
 
 
-void dim_lcd_task(void* param)
-{
+void dim_lcd_task(void* param){
     // check periodically if dimmer is not cancelled from outside       
     uint8_t i = 0;
-    while(core.dim_alive && (i < POWER_SAVE_TIME_SPLIT))
-    {
+    while(dim_alive && (i < POWER_SAVE_TIME_SPLIT)){
         vTaskDelay(((POWER_SAVE_TIME / POWER_SAVE_TIME_SPLIT) / portTICK_PERIOD_MS));
         i++;
     }
-    if(!core.dim_alive)
-    {
+    if(!dim_alive){
         vTaskDelete(NULL);
     }
-    else
-    {
-        mbox.push({.origin = ORG_SW, .content = STATE_ATTRIBUTE_LCD_DARK}, false);
-        mbox.notify(tLoop, false);
-        core.dim_alive = false;
+    else{
+        cmd_t c = {
+            .origin = ORG_SW, 
+            .content = STATE_ATTRIBUTE_LCD_DARK
+            };
+        task_notify(tLoop, c, false);
+        dim_alive = false;
         vTaskDelete(NULL);
     }
+}
+
+void task_notify(TaskHandle_t t, cmd_t c, bool from_isr){
+    if(from_isr){
+        BaseType_t dummy = pdFALSE;
+        xTaskNotifyFromISR(t, *(uint32_t*)&c, eSetValueWithOverwrite, &dummy);
+    }
+    else{
+        xTaskNotify(t, *(uint32_t*)&c, eSetValueWithOverwrite);
+    }
+}
+
+cmd_t task_notify_take(TickType_t ticks_to_wait){
+    uint32_t c = ulTaskNotifyTake(pdTRUE, ticks_to_wait);
+    return *(cmd_t*)&c;
 }
--- Code/lib/MAIN/tasks.h
+++ Code/lib/MAIN/tasks.h
@@ -3,23 +3,21 @@
 
 #include "Arduino.h"
 
+// command data type
+typedef struct {
+    uint8_t origin;
+    uint8_t content;
+    uint16_t pad;
+} cmd_t;
+
 void mainLoopTask(void* param);
-
-
 void webserverTask(void* param);
-
-
 void wsClientHandlerTask(void* param);
-
-
 void time_led_task(void* param);
-
-
 void blink_led_task(void* param);
-
-
 void dim_lcd_task(void* param);
-
+void IRAM_ATTR task_notify(TaskHandle_t t, cmd_t c, bool from_isr);
+cmd_t IRAM_ATTR task_notify_take(TickType_t ticks_to_wait);
 
 extern TaskHandle_t tLoop;
 extern TaskHandle_t tWebserver;
