90e91f222018248377817f082afd141133b175b1
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-25 17:48:44 +0200

    docsss

--- Code/lib/webserver/mywebserver.h
+++ Code/lib/webserver/mywebserver.h
@@ -8,7 +8,17 @@
 #define WEBSERVER_TIMEOUT   2000
 #define BUFLEN              30
 
+/// @brief Prints obtained IP to Serial
+/// @note Will only print useful information if device
+/// is connected to WiFi (duh)
 void mywebserver_printIP();
+
+/// @brief Initializes the webserver hosting the control page
+/// for clients within the same LAN
+/// @note Will block until connected, because this device is
+/// useless without internet connection. Check the folder
+/// "website" for its html, css and js content. Uses ajax for
+/// page updates.
 void mywebserver_init();
 
 #endif
