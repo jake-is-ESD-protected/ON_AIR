7d240ab1f4b8a2d293d191e6471436bcc0b7583e
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-25 17:29:33 +0200

    docsssss

--- Code/lib/MAIN/states.h
+++ Code/lib/MAIN/states.h
@@ -1,18 +1,13 @@
+/// @file states.h
+/// @brief The macros here are specifically made
+/// to be identifiable by value. Only this way a 
+/// complete exclusion of e.g. transitional states 
+/// can be achieved by simply checking "if(cmd < 100)"
+
 #ifndef _STATES_H_
 #define _STATES_H_
 
-// command IDs      
-#define BELL_INT                  103
-
-/*
-  The macros here are specifically made
-  to be identifiable by value. Only this
-  way a complete exclusion of e.g. tran-
-  sitional states can be achieved by
-  simply checking "if(cmd < 100)"
-*/
-
-// persistant states
+/// @brief Persistant states of FSM
 #define STATE_IDLE                0
 #define STATE_WOEX                1
 #define STATE_MEET                2
@@ -21,14 +16,14 @@
 #define STATE_WELC                5
 #define N_STATES                  6
 
-// transitional states
+/// @brief Transitional states of FSM
 #define STATE_TRANS_BORDER_C      10
 #define STATE_BELL                60
 #define STATE_NO_RESPONSE         70
 #define STATE_NO_STATE            80
 #define STATE_ERROR               90
 
-// state attributes
+/// @brief Additional state attributes
 #define STATE_ATTRIBUTE_BORDER_C  100
 #define STATE_ATTRIBUTE_LCD_DARK  100
 #define STATE_ATTRIBUTE_BL_ON     101
@@ -36,9 +31,12 @@
 #define STATE_ATTRIBUTE_LA_ON     103
 #define STATE_ATTRIBUTE_LA_OFF    104
 
-// command origins
-#define ORG_HW  0 // command stems from hardware (interrupt)
-#define ORG_SW  1 // command stems from software (task callback)
-#define ORG_WS  2 // command stems from browser-client (master device)
+/// @brief Command origins.
+/// ORG_HW stems from hardware (interrupt),
+/// ORG_SW stems from software (task callback)
+/// ORG_WS stems from browser-client (master device)
+#define ORG_HW  0
+#define ORG_SW  1
+#define ORG_WS  2
 
 #endif
--- Code/lib/UI/hardware.h
+++ Code/lib/UI/hardware.h
@@ -1,55 +1,50 @@
-/*
-*****************************************************************************************
-header name:        hardware.h
-auth:               Jakob T.
-date:               16.01.22
-brief:              pin definitions and board-specific info
-version:            V1.1
-*****************************************************************************************
-*/
-
 #ifndef _HARDWARE_H_
 #define _HARDWARE_H_
 
 #include "Arduino.h"
 
-// pins
+/// @brief Pin definitions specific to PCB
 #define BUTTON_LED_PIN            23
 #define LED_ARRAY                 18
 #define BUTTON_IN_PIN             19
 
-// digital debounce
-#define IR_DEBOUNCE_TIME          5000
-
-// timings                 
+/// @brief Timing macros for transitional jobs                 
 #define RING_TIME                 10000
 #define RING_AMOUNT               (RING_TIME / 2000)
 #define POWER_SAVE_TIME           10000
 #define POWER_SAVE_TIME_SPLIT     (POWER_SAVE_TIME / 1000)
 #define DOUBLE_MSG_DELAY          2000
+#define IR_DEBOUNCE_TIME          5000
 
-
-// interrupt service routine for button (bell)
+/// @brief Interrupt service routine for button (bell)
+/// @note Includes a digital debounce specified in the
+/// macro IR_DEBOUNCE_TIME
 void IRAM_ATTR BUT_ISR(void);
 
 
-// init ISR and queue
+/// @brief Init interrupt service routine for button
+/// @note Uses Arduino's attachInterrupt()
 void init_ISRs(void);
 
 
-// init all GPIOs as OUTPUT/INPUT
+/// @brief Init all GPIOs as OUTPUT/INPUT
+/// @note GPIOs in question are found as BUTTON_LED_PIN,
+/// LED_ARRAY and BUTTON_IN_PIN
 void init_gpios(void);
 
 
-// turn on LED-array
+/// @brief Turn on LED-array
+/// @note Wrapper for digitalWrite(), has effect on LED array
 void led_on(void);
 
 
-// turn off LED-array
+/// @brief Turn off LED-array
+/// @note Wrapper for digitalWrite(), has effect on LED array
 void led_off(void);
 
 
-// blink button LED by fixed amount
+/// @brief Blink button LED by fixed amount
+/// @note has effect on LED of button
 void led_blink(void);
 
 #endif
