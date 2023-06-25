5de88b7cd648c3a647f2710fd04f1a803bcaeb3d
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-25 11:38:57 +0200

    add flag and task handle setters/getters and cocs

--- Code/lib/MAIN/core.cpp
+++ Code/lib/MAIN/core.cpp
@@ -1,17 +1,11 @@
 #include "LiquidCrystal_I2C.h"
 #include "core.h"
-#include "states.h"
+#include "tasks.h"
 #include "hardware.h"
 #include "lcd.h"
-#include "tasks.h"
 
-bool tim_alive = false;
-bool dim_alive = false;
-bool led_alive = false;
-bool bell = false;
-
-TaskHandle_t tTim = NULL;
-TaskHandle_t tDim = NULL;
+bool flags[NUM_FLAGS] = {false};
+TaskHandle_t task_handles[NUM_TASKS] = {NULL};
 
 cmd_t last_cmd = {
     .origin = ORG_SW,
@@ -21,66 +15,79 @@ cmd_t last_cmd = {
 
 void handle_cmd(cmd_t inc_cmd)
 {
+    TaskHandle_t timer = get_task_handle(t_tim);
+    TaskHandle_t dimmer = get_task_handle(t_dim);
     /*  
         button has been pressed:
         - blink LED for RING_TIME ms
         - wait for client to pick up the bell-status
     */
-    if((inc_cmd.content == STATE_BELL) && !tim_alive)
-    {
-        bell = true;
+    if((inc_cmd.content == STATE_BELL) && !get_flag(f_timer_alive)){
         Serial.printf("[CORE]\tIn bell-handler\n");
-        dim_alive = false;
+        set_flag(f_bell_alive, true);
+        set_flag(f_dimmer_alive, false);
 
         xTaskCreate(time_led_task, 
             "start a timer which steers blinking for 5 s",
             1024,
             NULL,
             1,
-            &tTim);       
+            &timer);       
     }
 
     /*  
         device is in idle mode and has not yet started the dim-timer:
         - launch dimming-task
     */
-    if(inc_cmd.content == STATE_IDLE && !dim_alive)
-    {
+    if(inc_cmd.content == STATE_IDLE && !get_flag(f_dimmer_alive)){
         Serial.printf("[CORE]\tLaunch dimming task\n");
-        dim_alive = true;
+        set_flag(f_dimmer_alive, true);
         xTaskCreate(dim_lcd_task,
                     "go into power saving mode",
                     2048,
                     NULL,
                     1,
-                    &tDim);
+                    &dimmer);
     }
 
     /*  
         cancel ringing task if answer occurs from master
     */
-    if((inc_cmd.content < STATE_TRANS_BORDER_C) && tim_alive)
-    {
+    if((inc_cmd.content < STATE_TRANS_BORDER_C) && get_flag(f_timer_alive)){
         Serial.printf("[CORE]\tRegistered answer from client\n");
-        tim_alive = false;
+        set_flag(f_timer_alive, true);
     }
 
     /*  
         any action taken while device is entering dim-mode:
         - delete dim-timer
     */
-    if((inc_cmd.content != STATE_IDLE) && dim_alive)
+    if((inc_cmd.content != STATE_IDLE) && get_flag(f_dimmer_alive))
     {
         Serial.println("[CORE]\tStop dimming task\n");
-        dim_alive = false;
+        set_flag(f_dimmer_alive, false);
     }
 
     lcd.display_state(inc_cmd.content);
-    // Serial.printf("\ttim-task: %d, led-task: %d, dim-task: %d\r\n", tim_alive, led_alive, dim_alive);
 
     if(inc_cmd.content < STATE_TRANS_BORDER_C)          
     {   // skip any instances of transitional states
         Serial.printf("[CORE]\tentering state '%d'\r\n", inc_cmd.content);
         last_cmd = inc_cmd;
     }
+}
+
+bool get_flag(flag_t f){
+    // assert something idk
+    return flags[f];
+}
+
+void set_flag(flag_t f, bool mode){
+    // assert something idk
+    flags[f] = mode;
+}
+
+TaskHandle_t* get_task_handle(task_t t){
+    // assert something idk
+    return &task_handles[t];
 }
--- Code/lib/MAIN/core.h
+++ Code/lib/MAIN/core.h
@@ -1,19 +1,61 @@
 #ifndef _CORE_H_
 #define _CORE_H_
 
+#include <Arduino.h>
 #include "states.h"
-#include "tasks.h"
-#include "mywebserver.h"
 
+/// @brief Flag enumeration for flag selection and setting.
+typedef enum flag_t{
+    f_timer_alive,
+    f_dimmer_alive,
+    f_led_alive,
+    f_bell_alive,
+    NUM_FLAGS
+}flag_t;
+
+/// @brief Task enumeration for handle selection
+typedef enum task_t{
+    t_loop,
+    t_tim,
+    t_dim,
+    t_led,
+    NUM_TASKS
+}task_t;
+
+/// @brief Command struct with content and place of origin
+typedef struct cmd_t{
+    uint8_t origin;
+    uint8_t content;
+    uint16_t pad;
+} cmd_t;
+
+/// @brief Parse command and process it. Checks state of
+/// core and starts worker tasks if necessary.
+/// @param inc_cmd cmd_t, command to handle
 void handle_cmd(cmd_t inc_cmd);
 
-extern TaskHandle_t tLoop;
-extern TaskHandle_t tTim;
-extern TaskHandle_t tDim;
-extern bool tim_alive;
-extern bool dim_alive;
-extern bool led_alive;
-extern bool bell;
+/// @brief Obtain a specific flag from the core without
+/// accessing it directly.
+/// @param f flag_t, flag to read
+/// @return bool, status of flag
+bool get_flag(flag_t f);
+
+/// @brief Set a specific flag.
+/// @param f flag_t, flag to set
+/// @param mode bool, state of flag to set
+void set_flag(flag_t f, bool mode);
+
+/// @brief Get the task handle for a task.
+/// @param t task_t, task type to get
+/// @return TaskHandle_t*, pointer to handle of task
+/// @note FreeRTOS expects the pointer to a task handle
+/// in xTaskCreate(), but the handle itself for all other
+/// functionalities. This makes the return of this function
+/// a double pointer, to be exact void**. Don't forget to
+/// dereference its return value when passing it to functions
+/// like xTaskNotify().
+TaskHandle_t* get_task_handle(task_t t);
+
 extern cmd_t last_cmd;
 
 #endif
